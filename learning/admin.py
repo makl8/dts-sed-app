af5c67f01d69b834f35c7a410fc373c50a5e071b
makl8 <126243784+makl8@users.noreply.github.com>
2026-05-06 11:54:19 +0200

    Register models for admin site

---
+++ learning/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from learning.models import Course, Training
+
+admin.site.register(Course)
+
+
+@admin.register(Training)
+class TrainingAdmin(admin.ModelAdmin):
+    """Make training expiry date and date added visible in the admin as read-only."""
+
+    readonly_fields = (
+        "date_added",
+        "training_expiry_date",
+    )
