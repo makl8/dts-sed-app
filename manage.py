d8345ba5cbf15aecfa127cfab44c86b750dd7b1f
makl8 <126243784+makl8@users.noreply.github.com>
2026-05-11 00:25:39 +0100

    Fix isort issue

--- manage.py
+++ manage.py
@@ -10,7 +10,7 @@ def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning.settings")
     try:
         from django.core.management import (
-            execute_from_command_line  # pylint: disable=import-outside-toplevel
+            execute_from_command_line,  # pylint: disable=import-outside-toplevel
         )
     except ImportError as exc:
         raise ImportError(
